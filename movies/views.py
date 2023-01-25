fb4862316c6538eedbbfba2f31bfdaa15fc5cffb
Jedsada Luengaramsuk <68596277+jedsada-io@users.noreply.github.com>
2023-01-25 16:11:00 +0700

    Raising 404 and URLs referencing

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 #Import django https 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie 
 
 
@@ -11,11 +11,6 @@ def index (request):
     movies = Movie.objects.all()
     return(render(request, 'movies/index.html', {'movies' :movies }))
 
-def detail(request, movie_id): 
-    movie = Movie.objects.get(pk=movie_id)
-    return render(request, 'movies/detail.html', {'movie' : movie} )
-
-    
-
-
-#Adding URLs > Creating new urls.py in the app folders 
+def detail(request, movie_id):
+     movie =  get_object_or_404(Movie, pk=movie_id)
+     return render(request, 'movies/detail.html', {'movie' : movie} )
